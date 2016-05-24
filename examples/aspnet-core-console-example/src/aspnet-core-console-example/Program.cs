34891f9a6df6b4656d576971d735a4d058cb4191
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-05-25 00:06:15 +0200

    Added Console test (not finished)

---
+++ examples/aspnet-core-console-example/src/aspnet-core-console-example/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using NLog.Extensions.Logging;
+
+namespace aspnet_core_console_example
+{
+    public class Program
+    {
+        public Program(ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddNLog();
+        }
+
+        public static void Main(string[] args)
+        {
+            //DI won't work outside ASP.NET Core since RC2.
+
+            var services = new ServiceCollection();
+            var container = services.BuildServiceProvider();
+            var LoggerFactory = container.GetService<LoggerFactory>();
+
+            // PlatformServices.Default.Runtime.
+
+            Console.WriteLine("TEST");
+            Console.WriteLine("press any key to Exit");
+            Console.ReadLine();
+        }
+    }
+
+
+}
